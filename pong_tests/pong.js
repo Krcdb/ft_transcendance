f5dc0623117084591ae3cb57162587ed6fc4cf30
Mia-Cross <leila.marabese@gmail.com>
2021-10-13 18:49:15 +0200

    suppression de trucs qui n'auraient pas du quitter ma branche

--- pong_tests/pong.js
+++
@@ -1,5 +0,0 @@
-console.log('ok');
-const userBar = document.createElement('div');
-userBar.setAttribute('id', 'user-bar');
-userBar.setAttribute('style', 'width:10%; height:50px');
-userBar.style.display = block;
