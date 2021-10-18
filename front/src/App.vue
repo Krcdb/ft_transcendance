66bc54c785eaf38887489c7d6e82cfcd7758f138
Mia-Cross <leila.marabese@gmail.com>
2021-10-18 18:35:37 +0200

    saving what i did before the w-e [not working]

--- front/src/App.vue
+++ front/src/App.vue
@@ -23,7 +23,7 @@ export default defineComponent({
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 100px;
+  margin-top: 10%;
 }
 button {
   border: none;
---
+++ lemarabe_stuff/1634231436808-PostRefactoring.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class PostRefactoring1634231436808 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
