ba6405480d40d0b05bf5ad6c7917b497259f882d
Servane CHENE <schene@e3r6p11.42.fr>
2021-11-15 17:27:20 +0100

    login in vue component

--- front/src/statics/log.methods.ts
+++ front/src/statics/log.methods.ts
@@ -1,23 +1,4 @@
 import http from "@/http-common";
-// import ResponseData from "@/types/ResponseData";
-
-const login = {
-  url: "https://api.intra.42.fr/oauth/authorize?",
-  query: {
-    client_id: process.env.VUE_APP_FT_CLIENT_ID,
-    redirect_uri: process.env.VUE_APP_FT_REDIRECT_URL,
-    response_type: "code",
-  },
-  loginUser: function (): string {
-    localStorage.removeItem("user-name");
-    localStorage.removeItem("user-id");
-    localStorage.removeItem("user-token");
-    this.url += Object.entries(this.query)
-      .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
-      .join("&");
-    return this.url;
-  },
-};
 
 function logout(): void {
   const url = `http://localhost:3000/users/logout/${localStorage.getItem(
@@ -39,4 +20,4 @@ function logout(): void {
   window.location.assign("/login");
 }
 
-export { logout, login };
+export { logout };
