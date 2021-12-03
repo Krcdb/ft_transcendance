7417c6707123f39b0e87ca907587874940407738
Servane CHENE <schene@e3r6p11.42.fr>
2021-12-03 20:53:23 +0100

    no select profile in chat

--- front/src/views/Chat.vue
+++ front/src/views/Chat.vue
@@ -2,17 +2,17 @@
 	<BurgerMenu />
     
     <!-- FOR USER SELECTION -->
-    <OwnerProfile @getUserSelected="HandleGetUserSelected"/>
-    <PublicChannelList :user="user" v-if="this.userSelected"/>
+    <!-- <OwnerProfile @getUserSelected="HandleGetUserSelected"/>
+    <PublicChannelList :user="user" v-if="this.userSelected"/> -->
     
     <!-- NO USER SELECTION -->
-    <!-- <div v-if="isloading">
+    <div v-if="isloading">
         Loading...
     </div>
     <div v-else>
         <MyProfile :user="user" />
         <PublicChannelList :user="user" />
-    </div> -->
+    </div>
 
 </template>
 
